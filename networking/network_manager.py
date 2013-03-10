bb24cac57af94444e8eda694a0158bb3e0bee558
Mitchell Stokes <mogurijin@gmail.com>
2013-03-10 15:52:13 -0700

    Adding in some basic replication code from an earlier test (nothing's actually sent over the network yet).

--- networking/network_manager.py
+++ networking/network_manager.py
@@ -1,10 +1,30 @@
 from .network_handlers.enet_network_handler import ENetNetworkHandler
 
 
+def replicatable_class(cls):
+	def nm_setattr(self, name, value):
+		if name in self.replication_vars and hasattr(self, NetworkManager._var("dirty_set")):
+			getattr(self, NetworkManager._var("dirty_set")).add(name)
+		getattr(self, NetworkManager._var("__setattr__"))(name, value)
+	
+	# Make sure this is set to cls.__init__ has something to play with
+	setattr(cls, NetworkManager._var("dirty_set"), set())
+	
+	# Store the existing __setattr__
+	setattr(cls, NetworkManager._var("__setattr__"), cls.__setattr__)
+	
+	# Now override the classes __setattr__ with our own
+	cls.__setattr__ = nm_setattr
+	
+	return cls
+
+
 class NetworkManager:
 	
 	ROLES = {"CLIENT", "SERVER"}
 	HANDLER_CLASS = ENetNetworkHandler
+	
+	var_prefix = "_NM__"
 	
 	def __init__(self, role, host, port):
 		if role not in self.ROLES:
@@ -16,6 +36,12 @@ class NetworkManager:
 			self.handler = self.HANDLER_CLASS.create_server(host, port)
 		elif role == "CLIENT":
 			self.handler = self.HANDLER_CLASS.create_client(host, port)
+
+		self._actors = set()
+
+	@classmethod
+	def _var(cls, var):
+		return cls.var_prefix+var
 	
 	@classmethod
 	def create_server(cls, port):
@@ -28,6 +54,29 @@ class NetworkManager:
 	@property
 	def connected(self):
 		return self.handler.connected
+
+	def register_actor(self, actor):
+		if actor not in self._actors:
+			setattr(actor, self._var("dirty_set"), set())
+			
+			self._actors.add(actor)
+		else:
+			print("Warning, actor already added")
+	
+	def unregister_actor(self, actor):
+		if actor in self._actors:
+			self._actors.remove(actor)
+		else:
+			print("Warning, actor not registered")
 	
 	def run(self):
+		
+		# Find any dirty attributes that we need to replicate
+		for actor in self._actors:
+			for var in getattr(actor, self._var("dirty_set")):
+				print(actor.__class__.__name__, "dirty var", var, "value:", getattr(actor, var))
+
+			setattr(actor, self._var("dirty_set"), set())
+
+		# Process any events on the handler
 		self.handler.process_events()
